e4d51dd040e0e554df37c01df071f6c3165c80ec
langdonglei <langdonglei@icloud.com>
2021-06-20 22:15:07 +0800

    m

---
+++ sh/install_tool.sh
@@ -0,0 +1,9 @@
+dnf install -y \
+    net-tools \
+    unzip \
+    zip \
+    vim \
+    git \
+    gcc \
+    gcc-c++ \
+    wget
--- sh/run.sh
+++ sh/run.sh
@@ -1,1 +1,3 @@
+sh /root/install_tool.sh
 sh /root/install_nginx.sh
+sh /root/install_php.sh
