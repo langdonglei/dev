a11bb209b1a7f957be57ae8cee40a0bdb0b519c4
langdonglei <langdonglei@icloud.com>
2021-11-16 10:34:37 +0800

    fix

--- sh/install_php.sh
+++ sh/install_php.sh
@@ -1,6 +1,6 @@
 dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 
-
+# php73
 dnf install -y \
     php73-php-cli \
     php73-php-fpm \
@@ -20,16 +20,15 @@ display_errors=1
 date.timezone=PRC
 zend.assertions=1
 EOF
-cat >> /etc/opt/remi/php73/php-fpm.d/www.conf << EOF
-listen=127.0.0.1:9000
-EOF
 cat >> /etc/opt/remi/php73/php.d/15-xdebug.ini << EOF
 xdebug.mode=debug
 xdebug.client_host=192.168.1.222
 EOF
 sed -i 's|apache|root|g' /etc/opt/remi/php73/php-fpm.d/www.conf
 sed -i 's|nodaemonize|nodaemonize -R|' /lib/systemd/system/php73-php-fpm.service
+systemctl enable php73-php-fpm
 
+# php80
 dnf install -y \
     php80-php-cli \
     php80-php-fpm \
@@ -49,24 +48,18 @@ display_errors=1
 date.timezone=PRC
 zend.assertions=1
 EOF
-cat >> /etc/opt/remi/php80/php-fpm.d/www.conf << EOF
-listen=127.0.0.1:9000
-EOF
 cat >> /etc/opt/remi/php80/php.d/15-xdebug.ini << EOF
 xdebug.mode=debug
 xdebug.client_host=192.168.1.222
 EOF
 sed -i 's|apache|root|g' /etc/opt/remi/php80/php-fpm.d/www.conf
 sed -i 's|nodaemonize|nodaemonize -R|' /lib/systemd/system/php80-php-fpm.service
-
-
-
-
 systemctl enable php80-php-fpm
 
-ln -s /usr/bin/php80 /usr/local/bin/php
-
-
+# composer
 curl -L https://github.com/composer/composer/releases/download/2.1.3/composer.phar -o /usr/local/bin/composer
 chmod a+x /usr/local/bin/composer
 composer config -g repo.packagist composer https://mirrors.aliyun.com/composer
+
+# default php
+ln -s /usr/bin/php80 /usr/local/bin/php
--- sh/run.sh
+++ sh/run.sh
@@ -3,4 +3,3 @@ sh /sh/install_nginx.sh
 sh /sh/install_php.sh
 sh /sh/install_mysql.sh
 sh /sh/install_redis.sh
-sh /sh/install_postgresql.sh
