5abf7ee32cca9dfed2b30e6ca2ddba3dbe81d372
langdonglei <langdonglei@icloud.com>
2021-12-03 10:35:45 +0800

    m

--- sh/install_php.sh
+++ sh/install_php.sh
@@ -61,5 +61,6 @@ curl -L https://github.com/composer/composer/releases/download/2.1.3/composer.ph
 chmod a+x /usr/local/bin/composer
 composer config -g repo.packagist composer https://mirrors.aliyun.com/composer
 
-# default php
+# default cli php
+#ln -s /usr/bin/php73 /usr/local/bin/php
 ln -s /usr/bin/php80 /usr/local/bin/php
