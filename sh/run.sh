e0b786ca7e3e0219f47500bac91da14ef12f9da3
langdonglei <30425179+langdonglei@users.noreply.github.com>
2022-01-03 22:15:20 +0800

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,4 +1,4 @@
-
+dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 dnf install -y net-tools unzip zip vim git gcc gcc-c++ sshpass wget
 #sh /sh/install_nginx.sh
 #sh /sh/install_php.sh
