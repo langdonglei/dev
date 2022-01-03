533ea5f0dac3c6798d9b9ff7f821a2094b731dd7
langdonglei <30425179+langdonglei@users.noreply.github.com>
2022-01-03 21:33:01 +0800

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,5 +1,5 @@
 sh /sh/install_tool.sh
-sh /sh/install_nginx.sh
-sh /sh/install_php.sh
-sh /sh/install_mysql.sh
-sh /sh/install_redis.sh
+#sh /sh/install_nginx.sh
+#sh /sh/install_php.sh
+#sh /sh/install_mysql.sh
+#sh /sh/install_redis.sh
