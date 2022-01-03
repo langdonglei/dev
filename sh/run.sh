2fd8cb32cc888d98e58736ac25bf68e11ac13848
langdonglei <30425179+langdonglei@users.noreply.github.com>
2022-01-03 22:17:56 +0800

    Update run.sh

--- sh/run.sh
+++ sh/run.sh
@@ -1,6 +1,5 @@
+dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
 dnf install -y net-tools unzip zip vim git gcc gcc-c++ sshpass wget
-#sh /sh/install_nginx.sh
-#sh /sh/install_php.sh
-#sh /sh/install_mysql.sh
-#sh /sh/install_redis.sh
+sh /sh/install_nginx.sh
+sh /sh/install_php.sh
