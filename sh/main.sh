416679fbb7a3c90445eb1618c905185508663816
langdonglei <langdonglei@icloud.com>
2022-01-31 22:15:35 +0800

    fix

--- sh/main.sh
+++ sh/main.sh
@@ -1,6 +1,6 @@
 dnf install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 dnf install -y https://rpms.remirepo.net/enterprise/remi-release-8.rpm
-dnf install -y net-tools unzip zip vim git gcc gcc-c++ sshpass wget
+dnf install -y net-tools unzip zip vim git gcc gcc-c++ sshpass wget p7zip
 
 sh /sh/install_nginx.sh
 sh /sh/install_redis.sh
