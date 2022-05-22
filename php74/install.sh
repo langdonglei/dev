b7f74ff9dd6eae3e2e53e48b2ffb87c33fc21f04
langdonglei <langdonglei@icloud.com>
2022-05-22 14:07:10 +0800

    remove git

--- php74/install.sh
+++ php74/install.sh
@@ -1,7 +1,7 @@
 rm -rf /etc/yum.repo.d/*.repo
 curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y net-tools unzip zip vim git gcc gcc-c++ sshpass wget p7zip
+yum install -y net-tools unzip zip vim gcc gcc-c++ sshpass wget p7zip
 
 sh /install/install_epel_redis3.sh
 sh /install/install_remi_php74.sh
