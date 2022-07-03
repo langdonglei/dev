474847b6b33684fe8bd43f896b0504bb49725a4a
langdonglei <langdonglei@icloud.com>
2022-07-03 19:50:40 +0800

    2

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 set -ex
-rm -rf /etc/yum.repo.d/*.repo
-curl -o /etc/yum.repos.d/CentOS-Base.repo https://mirrors.aliyun.com/repo/Centos-7.repo
+
 yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
 yum install -y git net-tools unzip zip vim gcc gcc-c++ sshpass wget p7zip psmisc
 
 sh /install/install_epel_redis3.sh
