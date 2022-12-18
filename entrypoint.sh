517c44660a5cd8b34703da3e1acdccf9529cd540
langdonglei <30425179+langdonglei@users.noreply.github.com>
2022-12-18 12:24:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-nginx && php72-fpm -R && php73-fpm -R && php74-fpm -R && php80-fpm -R && redis-server /etc/redis.conf
+/usr/sbin/sshd && nginx && php72-fpm -R && php73-fpm -R && php74-fpm -R && php80-fpm -R && redis-server /etc/redis.conf
