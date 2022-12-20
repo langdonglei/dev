8ad11b5a6da134e5fb1478d00e0c67acc4bb8202
langdonglei <langdonglei@icloud.com>
2022-12-20 16:27:22 +0800

    e

--- install_openssh.sh
+++ install_openssh.sh
@@ -1,3 +1,6 @@
 yum install -y openssh-server sshpass passwd
-ssh-keygen -A
+/usr/bin/ssh-keygen -A
 echo root:root | chpasswd
+sed -i 's|GSSAPIAuthentication yes|GSSAPIAuthentication no|' /etc/ssh/sshd_config
+sed -i 's|#UseDNS yes|UseDNS no|' /etc/ssh/sshd_config
+sed -i 's|#PermitRootLogin yes|PermitRootLogin yes|' /etc/ssh/sshd_config
