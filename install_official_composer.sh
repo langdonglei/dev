f132abedfa6db878e0544dadeff5fd4c63131cfa
langdonglei <30425179+langdonglei@users.noreply.github.com>
2023-07-23 01:12:40 +0800

    Update install_official_composer.sh

--- install_official_composer.sh
+++ install_official_composer.sh
@@ -1,3 +1,3 @@
 curl -L https://github.com/composer/composer/releases/download/2.1.3/composer.phar -o /usr/local/bin/composer
 chmod a+x /usr/local/bin/composer
-composer config -g repo.packagist composer https://mirrors.aliyun.com/composer
+#composer config -g repo.packagist composer https://mirrors.aliyun.com/composer
