84d9f8fbf6c9264e6bc3d29dd19159cafd2d21ff
langdonglei <30425179+langdonglei@users.noreply.github.com>
2023-08-06 22:17:33 +0800

    v

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-/usr/sbin/sshd && nginx && php73-fpm -R && php74-fpm -R && php80-fpm && && php81-fpm -R && redis-server /etc/redis.conf
+redis-server /etc/redis.conf && nginx && php74-fpm -R && php80-fpm && && php81-fpm -R
