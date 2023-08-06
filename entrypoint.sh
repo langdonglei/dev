bb8f9e706c62baa7e8724aaca942a6d7b81f6882
langdonglei <30425179+langdonglei@users.noreply.github.com>
2023-08-06 22:37:24 +0800

    v

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,1 @@
-redis-server /etc/redis.conf && nginx && php74-fpm -R && php80-fpm && && php81-fpm -R
+redis-server /etc/redis.conf && nginx && php74-fpm -R
--- install_remi_php.sh
+++ install_remi_php.sh
@@ -1,5 +1,5 @@
 yum install -y https://rpms.remirepo.net/enterprise/remi-release-7.rpm
-arr=( 74 80 81 )
+arr=( 74 )
 for i in ${arr[@]}
 do
     yum install -y \
