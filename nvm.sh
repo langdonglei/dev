d6ee927699e985f747570a602209b378302c51c7
langdonglei <30425179+langdonglei@users.noreply.github.com>
2024-01-04 20:56:45 +0800

    v

--- nvm.sh
+++ nvm.sh
@@ -3,5 +3,5 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 source "$NVM_DIR/nvm.sh"
 source "$NVM_DIR/bash_completion"
-nvm install v16.20.1
-nvm use v16.20.1
+nvm install 18
+nvm use 18
