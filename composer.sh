1a59199c850b9d287d73798bee6887759b8d623a
langdonglei <30425179+langdonglei@users.noreply.github.com>
2024-01-04 20:53:25 +0800

    v

--- composer.sh
+++ composer.sh
@@ -1,4 +1,4 @@
 set -ex
-curl -L https://github.com/composer/composer/releases/download/2.1.3/composer.phar -o /usr/local/bin/composer
+curl -L https://github.com/composer/composer/releases/download/2.6.6/composer.phar -o /usr/local/bin/composer
 chmod a+x /usr/local/bin/composer
 #composer config -g repo.packagist composer https://mirrors.aliyun.com/composer
