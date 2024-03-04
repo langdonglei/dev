bf738d858af52b43a7dbe1e28cae3f8e10006b63
langdonglei <30425179+langdonglei@users.noreply.github.com>
2024-03-04 09:40:35 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,12 @@
+
 nginx
 
-if test $1 == 80 ; then
-  /opt/remi/php80/root/sbin/php-fpm -R
-else
+if test $1 == 73 ; then
+  /opt/remi/php73/root/sbin/php-fpm -R
+elif test $1 == 74 ; then
   /opt/remi/php74/root/sbin/php-fpm -R
+else
+  /opt/remi/php80/root/sbin/php-fpm -R
 fi
 
 redis-server /etc/redis.conf
