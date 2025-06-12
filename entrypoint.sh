e0d4079fae3040607e20dff88f199e2c289040cc
langdonglei <langdonglei@icloud.com>
2025-06-12 13:27:06 +0800

    chore(entrypoint): 调整 PHP 版本启动逻辑并优化脚本结构
    - 重新组织 PHP 版本启动逻辑，使用 if-elif-else 结构
    - 添加注释以解释每个服务的启动顺序
    -调整 nginx 和 redis 的启动位置，使其与 PHP 启动逻辑分离

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,18 @@
-
+# 启动 nginx
 nginx
 
+# 启动 redis
+redis-server /etc/redis.conf
+
+# 启动 php-fpm
 if test $1 == 73 ; then
   /opt/remi/php73/root/sbin/php-fpm -R
-elif test $1 == 74 ; then
-  /opt/remi/php74/root/sbin/php-fpm -R
-else
-  /opt/remi/php80/root/sbin/php-fpm -R
 fi
 
-redis-server /etc/redis.conf
+if test $1 == 74 ; then
+  /opt/remi/php74/root/sbin/php-fpm -R
+fi
+
+if test $1 == 80 ; then
+  /opt/remi/php80/root/sbin/php-fpm -R
+fi
